255d11024b17a39e2359180a131140c63dfa71dd
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-05-26 00:36:41 -0400

    feat: add loadProto utility for loading gRPC service definitions

---
+++ usersService/src/utils/loadProto.js
@@ -0,0 +1,19 @@
+const { loadPackageDefinition } = require("@grpc/grpc-js");
+const { protoLoader } = require("@grpc/proto-loader");
+const path = require("path");
+
+function loadProto(serviceName) {
+    const protoPath = path.join("src", "protos", `${serviceName}.proto`);
+    
+    const packageDefinition = protoLoader.loadSync(protoPath, {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+    });
+
+    return loadPackageDefinition(packageDefinition)[serviceName];
+}
+
+module.exports = loadProto;
