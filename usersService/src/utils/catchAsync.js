63fd2c0e922c5ed3a742523a5a4363246ba87694
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-05-26 00:36:33 -0400

    feat: implement catchAsync utility for handling asynchronous errors

---
+++ usersService/src/utils/catchAsync.js
@@ -0,0 +1,13 @@
+function catchAsync(fn) {
+    return (call, callback) => {
+        fn(call, callback).catch(error => {
+            console.log(error);
+            callback({
+                code: error?.code || 13,
+                message: error?.message || 'Internal server error'
+            })
+        });
+    };
+}
+
+module.exports = catchAsync;
