7ba5e301fbdf374b6c2425ba2860549c74a4d9a4
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-06-05 01:31:16 -0400

    feat: update ListUsers RPC to require authentication and modify user role checks in userService

--- usersService/src/services/userService.js
+++ usersService/src/services/userService.js
@@ -1,14 +1,14 @@
 const bcrypt = require('bcrypt');
 const Users = require('../database/usersModel');
 const catchAsync = require('../utils/catchAsync');
-//const { getChannel, EXCHANGE_NAME } = require('../queue/config/connection');
+const { getChannel, EXCHANGE_NAME } = require('../queue/config/connection');
 
 const CreateUser = catchAsync(async (call, callback) => {
-    const { name, lastName, email, password, confirmationPassword, role } = call.request;
+    const { userId, name, lastName, email, password, confirmationPassword, role } = call.request;
     if (!name?.trim() || !lastName?.trim() || !email?.trim() || !password?.trim() || !confirmationPassword?.trim() || !role?.trim()) {
         return callback(new Error('Todos los campos son obligatorios'));
     }
-    
+
     const existingUser = await Users.findOne({email});
     if (existingUser) {
         return callback(new Error('El correo electrónico ya está en uso'));
@@ -21,6 +21,14 @@ const CreateUser = catchAsync(async (call, callback) => {
     if ( confirmationPassword !== password ) {
         return callback(new Error('Las contraseñas no coinciden'));
     }
+
+    const user = await Users.findById(userId);
+    if (role === 'Administrador') {
+        if (!userId || user.role !== 'Administrador') {
+            return callback(new Error('Solo un usuario autenticado con rol "Administrador" puede crear otro administrador'));
+        }
+    }
+  
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser = await Users.create({
         name,
@@ -49,20 +57,23 @@ const CreateUser = catchAsync(async (call, callback) => {
         name: newUser.name,
         lastName: newUser.lastName,
         email: newUser.email,
-        password: undefined,
         role: newUser.role,
         createdAt: newUser.createdAt.toISOString(),
-        
     });
 });
 
 const GetUser = catchAsync(async (call, callback) => {
-    const { id } = call.request;
-
+    const { userId, id } = call.request;
+    if (!userId) {
+        return callback(new Error('Debes estar autenticado para obtener un usuario'));
+    }
     const user = await Users.findById(id);
-
     if (!user) {
         return callback(new Error('Usuario no encontrado'));
+    }
+    const authenticatedUser = await Users.findById(userId);
+    if (authenticatedUser.role !== 'Administrador' && authenticatedUser.id !== user.id) {
+        return callback(new Error('No tienes permiso para ver este usuario'));
     }
 
     callback(null, {
@@ -76,21 +87,25 @@ const GetUser = catchAsync(async (call, callback) => {
 });
 
 const UpdateUser = catchAsync(async (call, callback) => {
-    const { id } = call.request;
+    const { userId, id, name, lastName, email, password} = call.request;
+
+    if (!userId) {
+        return callback(new Error('Debes estar autenticado para actualizar un usuario'));
+    }
 
     const user = await Users.findById(id);
-
     if (!user) {
         return callback(new Error('Usuario no encontrado'));
     }
-
-    const { password } = call.request;
 
     if (password) {
         return callback(new Error('No se puede modificar la contraseña aquí'));
     }
 
-    const { name, lastName, email } = call.request;
+    const authenticatedUser = await Users.findById(userId);
+    if (authenticatedUser.role !== 'Administrador' && authenticatedUser.id !== user.id) {
+        return callback(new Error('No tienes permiso para actualizar este usuario'));
+    }
 
     await Users.findOneAndUpdate(
       { _id: id },
@@ -119,12 +134,20 @@ const UpdateUser = catchAsync(async (call, callback) => {
 });
 
 const DeleteUser = catchAsync(async (call, callback) => {
-    const { id } = call.request;
+    const { userId, id } = call.request;
+
+    if (!userId) {
+        return callback(new Error('Debes estar autenticado para eliminar un usuario'));
+    }
 
     const user = await Users.findById(id);
-    
     if (!user) {
         return callback(new Error('Usuario no encontrado'));
+    }
+
+    const authenticatedUser = await Users.findById(userId);
+    if (authenticatedUser.role !== 'Administrador') {
+        return callback(new Error('No tienes permiso para eliminar un usuario'));
     }
 
     await Users.findOneAndUpdate(
@@ -143,12 +166,20 @@ const DeleteUser = catchAsync(async (call, callback) => {
       }))
     );
 
-
     callback(null, {});
 });
 
 const ListUsers = catchAsync(async (call, callback) => {
-    const { email, name, lastName } = call.request;
+    const { userId, email, name, lastName } = call.request;
+
+    if (!userId) {
+        return callback(new Error('Debes estar autenticado para listar usuarios'));
+    }
+
+    const authenticatedUser = await Users.findById(userId);
+    if (authenticatedUser.role !== 'Administrador') {
+        return callback(new Error('No tienes permiso para listar usuarios'));
+    }
 
     const query = { active: true };
 
@@ -174,6 +205,7 @@ const ListUsers = catchAsync(async (call, callback) => {
         role: user.role,
         createdAt: user.createdAt.toISOString(),
     }));
+
     callback(null, { users: result });
 });
 
