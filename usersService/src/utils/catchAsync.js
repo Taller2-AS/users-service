7a73cb1d7965c51c5648857cf012ffd0d03ffd15
xSharkz <105187265+xSharkz@users.noreply.github.com>
2025-06-11 17:22:41 -0400

    feat: enhance error handling in catchAsync to include detailed logging

--- usersService/src/utils/catchAsync.js
+++ usersService/src/utils/catchAsync.js
@@ -1,13 +1,27 @@
+const publishLog = require('../queue/publisher/logPublisher');
+
 function catchAsync(fn) {
-    return (call, callback) => {
-        fn(call, callback).catch(error => {
-            console.log(error);
-            callback({
-                code: error?.code || 13,
-                message: error?.message || 'Internal server error'
-            })
+  return (call, callback) => {
+    fn(call, callback).catch(async (err) => {
+      try {
+        await publishLog('error', {
+          userId: null,
+          email: '',
+          error: err.message,
+          date: new Date().toISOString()
         });
-    };
+      } catch (logError) {
+        console.error('Error al publicar en el log:', logError);
+      }
+
+      console.error(err);
+
+      callback({
+        code: err?.code || 13,
+        message: err?.message || 'Internal server error'
+      });
+    });
+  };
 }
 
 module.exports = catchAsync;
